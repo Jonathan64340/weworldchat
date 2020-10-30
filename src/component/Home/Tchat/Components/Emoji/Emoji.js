ab09471574a7108405bc29665e60bcf206edb66e
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2020-10-30 23:10:37 +0100

    Meilleur affichage des emoji

--- src/component/Home/Tchat/Components/Emoji/Emoji.js
+++ src/component/Home/Tchat/Components/Emoji/Emoji.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Button, Dropdown, Menu } from 'antd';
 import Picker, { SKIN_TONE_LIGHT } from 'emoji-picker-react';
 import { SmileOutlined } from '@ant-design/icons';
+import './emoji.css';
 
 const Emoji = ({ onEmojiChoose }) => {
     const [open, setOpen] = useState(false);
@@ -11,19 +12,16 @@ const Emoji = ({ onEmojiChoose }) => {
     }
 
     const emojiMenu = () => {
-        return <Menu>
-            <Menu.Item>
-                {open && <Picker onEmojiClick={chooseEmoji} groupNames={{
-                    smileys_people: 'yellow faces'
-                }} disableSearchBar className="emoji-select" skinTone={SKIN_TONE_LIGHT} />}
-            </Menu.Item>
-        </Menu>
+        return <>
+            {open && <Picker onEmojiClick={chooseEmoji} groupNames={{
+                smileys_people: 'yellow faces'
+            }} disableSearchBar className="emoji-select" skinTone={SKIN_TONE_LIGHT} />}
+        </>
     }
 
     return (<>
-        <Dropdown trigger={['click']} overlay={emojiMenu}>
-            <Button size="large" type="text" onClick={() => setOpen(true)} icon={<SmileOutlined />} />
-        </Dropdown>
+        <Button size="large" type="text" onClick={() => setOpen(!open)} icon={<SmileOutlined />} />
+        {emojiMenu()}
     </>)
 };
 
