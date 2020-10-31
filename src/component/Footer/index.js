7f709b6307019b0cd6b32b28cef7438b96b09c86
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2020-10-31 17:35:27 +0100

    a

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -6,7 +6,7 @@ import './footer.css';
 const Footer = () => {
     const toggleClass = useRef();
     const onToggle = () => {
-        console.log(toggleClass.current.classList.toggle('active'))
+        toggleClass.current.classList.toggle('active')
     }
     return (<div className="footer-content" ref={toggleClass}>
         <div className="footer-content-btn" onClick={() => onToggle()}>
