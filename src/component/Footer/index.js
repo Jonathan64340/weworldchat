a79bf2da869ac66173b6ee7a1f2c8bfe2d8cd744
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2020-11-01 12:00:44 +0100

    afficher correctement les mentions du footer

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -5,14 +5,20 @@ import './footer.css';
 
 const Footer = () => {
     const toggleClass = useRef();
+    const toggleHeight = useRef();
     const onToggle = () => {
         toggleClass.current.classList.toggle('active')
+        if(toggleClass.current.classList.contains('active')) {
+            toggleClass.current.style.bottom = `-${toggleHeight.current.clientHeight}px`;
+        } else {
+            toggleClass.current.style.bottom = `0`;
+        }
     }
     return (<div className="footer-content" ref={toggleClass}>
         <div className="footer-content-btn" onClick={() => onToggle()}>
             <UpOutlined />
         </div>
-        <div className="footer-content-description">
+        <div className="footer-content-description" ref={toggleHeight}>
             <div className="footer-content-description-info">
                 <InfoCircleOutlined />{' '}Ce chat ne collecte aucune information sensible. <br /> Toutes les données échangées au cours d'une discussion seront automatiquement supprimées une fois que l'utilisateur se déconnectera de l'application.
             </div>
