a126cbd63327d8fb52b2dadc06f28583b2dac563
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2021-04-04 12:30:04 +0200

    Add youtube iframe link watchable

--- src/component/Home/MessageContent/CustomRenderElement.js
+++ src/component/Home/MessageContent/CustomRenderElement.js
@@ -12,6 +12,23 @@ const CustomRenderElement = ({ string }) => {
   );
 
   return reactStringReplace(string, regex, (match, i, offset) => {
+    if (string.match('youtube' || 'youtu.be')) {
+      let video_id = match.split('v=')[1];
+      if (video_id) {
+        const amp = video_id.indexOf('&');
+        amp !== -1 && (video_id = video_id.substring(0, amp));
+        return <div className="video-wrapper">
+          <div className="video">
+            <iframe
+              title="Vidéo"
+              className="video-frame"
+              src={`https://www.youtube.com/embed/${video_id}`}
+              allowFullScreen
+            />
+          </div>
+        </div>
+      }
+    }
     return <a className="link-message" href={match} rel="noopener noreferrer" target="_blank">{match}</a>;
   });
 };
