88ce56ee47f77a56491f55342774f3f7e76dc381
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2021-04-13 08:24:46 +0200

    replace loader

--- src/component/Loader/Loader.js
+++ src/component/Loader/Loader.js
@@ -4,27 +4,14 @@ import PacmanLoader from 'react-spinners/PacmanLoader'
 import { connect } from 'react-redux';
 
 const Loader = ({ app, ...props }) => {
-    // Make blur behind the loader
-    const layout = document.getElementsByClassName('global-layout');
     if (app?.isLoading) {
-        layout && layout[0] && (layout[0].style.filter = 'blur(4px)')
+        document.body.style.cursor = "wait";
     } else {
-        layout && layout[0] && (layout[0].style.filter = 'blur(0)')
+        document.body.style.cursor = "default";
     }
 
     // Render
-    return <LoadingOverlay
-        styles={{
-            overlay: (base) => ({
-                ...base,
-                background: 'linear-gradient(48deg, rgba(36,36,0,1) 0%, rgba(0,6,162,0.6250875350140056) 0%, rgba(166,191,186,1) 100%)'
-            })
-        }}
-        active={app?.isLoading}
-        spinner={<PacmanLoader />}
-    >
-        {props.children}
-    </LoadingOverlay>
+    return <>{props.children}</>
 }
 
 const mapStateToProps = ({ app }) => ({ app });
